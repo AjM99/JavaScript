058eaf3d130c1f047da3068bee43a0cd4e873e54
AnujMore <47038171+AjM99@users.noreply.github.com>
2020-06-15 13:06:04 +0530

    Delete coersion.js

--- coersion.js
+++
@@ -1,6 +0,0 @@
-// coersion are basicaly prediction made by the JavaScript
-// run the follwing ans wer will be unexpected
-let a = 1 + true
-let b = 1 + false
-let c = '5' + 5         
-let d =  '5' - 5 
